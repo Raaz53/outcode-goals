8c8cfebf8f96eed8ede223a99342a4ec2c183b8a
Raaz53 <81346299+Raaz53@users.noreply.github.com>
2026-05-06 16:10:56 +0545

    Add files via upload

---
+++ 2026/Q2/taskManagerApp/app/(tabs)/index.tsx
@@ -0,0 +1,86 @@
+import { TaskContext } from "@/contexts/TaskContext";
+import { useRouter } from "expo-router";
+import { useContext } from "react";
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function HomeScreen(){
+  const router = useRouter();
+  const {tasks, setTasks} = useContext(TaskContext);
+
+  const deleteTask = (id: string) => {
+    setTasks((prev: any[]) => prev.filter(task => task.id !== id));
+  };
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Task List</Text>
+      <FlatList
+      data={tasks}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={styles.list}
+      renderItem={({item})=>(
+        <View style={styles.taskItem}>
+          <Text style={[styles.taskText, item.completed && styles.completedText]}>{item.title}</Text>
+          <TouchableOpacity onPress={() => deleteTask(item.id)}>
+            <Text style={styles.deleteBtn}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      />
+      <Button 
+      title="Add Task"
+      onPress={()=> router.push('/add-task')}
+      />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 20,
+  },
+  list:{
+    paddingBottom: 20,
+  },
+  taskItem: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  
+  taskText: {
+    fontSize: 16,
+  },
+  
+  completedText: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
+  },
+  
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 8,
+  },
+  
+  doneBtn: {
+    color: 'green',
+  },
+  
+  deleteBtn: {
+    color: 'red',
+  },
+});
